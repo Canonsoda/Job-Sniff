7821d58b3a843397c556afd246b441f0b330b405
mindlesssapien <sapienix@proton.me>
2025-06-30 05:28:34 +0530

    T_T

---
+++ cv_llm_scoring/model_loader.py
@@ -0,0 +1,50 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers.pipelines import pipeline
+
+MODEL_NAME = "microsoft/phi-2"
+
+def load_model():
+    LOCAL_MODEL_PATH = "/home/sapien/dev/projects/Job-Sniff/cv_llm_scoring/models/phi-2"
+    tokenizer = AutoTokenizer.from_pretrained(
+        LOCAL_MODEL_PATH,
+        local_files_only=True,
+        trust_remote_code=True
+    )
+
+    model = AutoModelForCausalLM.from_pretrained(
+        LOCAL_MODEL_PATH,
+        local_files_only=True,
+        trust_remote_code=True,
+        device_map="auto"
+    )
+
+    pipe = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        device_map="auto",
+        torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32
+    )
+    return pipe
+
+model_pipe = load_model()
+
+# def generate_json(resume_text: str):
+#     prompt = """
+# You are a resume parser.
+
+# Extract the following from the text below in **JSON** format:
+# - Name
+# - Email
+# - Phone
+# - Education (degree, institute, year)
+# - Work Experience (title, company, duration)
+# - Skills
+
+# Resume:
+# \"\"\"{resume_text}\"\"\"
+# Only return JSON.
+# """
+#     result = model_pipe(prompt, max_new_tokens=1024, do_sample=False)[0]['generated_text']
+#     return result
